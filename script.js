2d7eb03126f5662c120653138dbc9b73be551189
mowliidmohamed <71205472+mowliidmohamed@users.noreply.github.com>
2025-12-28 15:35:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -210,3 +210,27 @@ function openZoom(src) {
     }
     zoom.style.display = 'flex';
 }
+let deferredPrompt;
+const installBtn = document.getElementById('pwaInstallBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      }
+      deferredPrompt = null;
+      installBtn.style.display = 'none';
+    });
+  });
+});
